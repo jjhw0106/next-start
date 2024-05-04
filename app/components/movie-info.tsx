32159580a378c2b336594d52021821b4d5ddbd60
정호원 <jjhw0106@naver.com>
2024-05-04 20:18:00 +0900

    Feat:movie info

--- app/components/movie-info.tsx
+++ app/components/movie-info.tsx
@@ -1,8 +1,28 @@
+// import { API_URL } from "../constants";
+
+// async function getMovieInfo(id: string) {
+//   console.log("getMovieInfo!!!!!!!!!!!!!!!");
+//   await new Promise((resolve) => setTimeout(resolve, 13000));
+//   const response = await fetch(`${API_URL}/${id}`)
+//   return response.json();
+// }
+// // async function getMovieInfo(id: string) {
+// //   console.log("getMovieInfo!!!!!!!!!!!!!!!");
+// //   await new Promise((resolve) => setTimeout(resolve, 3000));
+// //   return [1,2,3,4]
+// // }
+
+// export default async function MovieInfo({id}: {id: string}) {
+//   const movieInfo = await getMovieInfo(id);
+//   return <h6>{JSON.stringify(movieInfo)}</h6>
+// }
+
+import styles from "../../styles/movie-info.module.css";
 import { API_URL } from "../constants";
 
-async function getMovieInfo(id: string) {
+export async function getMovieInfo(id: string) {
   console.log("getMovieInfo!!!!!!!!!!!!!!!");
-  await new Promise((resolve) => setTimeout(resolve, 13000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   const response = await fetch(`${API_URL}/${id}`)
   return response.json();
 }
@@ -14,5 +34,13 @@ async function getMovieInfo(id: string) {
 
 export default async function MovieInfo({id}: {id: string}) {
   const movieInfo = await getMovieInfo(id);
-  return <h6>{JSON.stringify(movieInfo)}</h6>
+  return <div className={styles.container}>
+    <img src={movieInfo.poster_path} className={styles.poster}/>
+    <div className={styles.info}>
+      <h1 className={styles.title}>{movieInfo.title}</h1>
+      <h3>💛{movieInfo.vote_average.toFixed(1)}</h3>
+      <p>{movieInfo.overview}</p>
+      <a href={movieInfo.homepage} target="_blank">Homepage &rarr;</a>
+    </div>
+  </div>
 }
